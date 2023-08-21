b5046284be50b1f7bad73cb4d7649787b5354abb
Joshua Smith <ws.joshua@gmail.com>
2023-08-21 13:14:37 -0600

    add object rest notes

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -4,7 +4,7 @@ export default function ToDoItem( {todo, index, ...props} ){
     {/* props - an interface for provide data scoped at a parent component to the child component */}
     // prop destructuring is a convenient way to access prop data without having to write 'props.prop1', 'props.prop2, etc. 
 
-    console.log(props)
+    console.log("testing rest operator - all none destructured props", props)
     console.log("testing props", todo)
     // we will evaluate the index prop -> if even provide one style, if odd another style  rule
 
