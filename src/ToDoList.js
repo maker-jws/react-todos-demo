5da38c105b64ec3e081776bf6af7575f76fffedc
Joshua Smith <ws.joshua@gmail.com>
2023-08-21 13:19:32 -0600

    update notes

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -12,10 +12,12 @@ function ToDoList({ todos, listName }) {
   // props.todos[0] -> { todos: [...]}
   // object destructuring -> {variable} -> JS
 
-  // Array.map - instance method -> todos -> create an array of JSX elements
+  // Array.map - instance method -> todos -> create an array of JSX elements from a source array
   const toDoListItems = todos.map(function (todoStr, idx) {
-    // console.log(idx, todoStr)
-    return <ToDoItem todo={todoStr} index={idx} />;
+    
+    // Important: "key" attributes for iterators are recommended to optimize react's virtual dom diff checks
+    // "keys" should be unique and not change between re-renders
+    return <ToDoItem todo={todoStr} index={idx} key={idx+todoStr.slice(0,3)} />;
   });
   
   // .map -> array method -> always returns a new array of values for each element in the provide/source array.
