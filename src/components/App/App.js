62129509442303bf24f54774d8c6e4afba2afb3f
Joshua Smith <ws.joshua@gmail.com>
2023-08-23 13:23:33 -0600

    updateadd todo content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,42 @@
 import "./App.css";
 
-import {useState} from 'react'
+import { useState } from "react";
 
 import ToDoList from "../ToDoList";
-
+import NewTodoForm from "../NewTodoForm";
 function App() {
-
-  const [todos, setTodos]=useState(["Learn React", "Learn the MERN Stack", "Have Fun"]);
-  const [showTodos, setShowTodos]=useState(true)
+  const [todos, setTodos] = useState([
+    "Learn React",
+    "Learn the MERN Stack",
+    "Have Fun",
+  ]);
+  
+  const [showTodos, setShowTodos] = useState(true);
+  
+  function handleHideClick (){
+    setShowTodos(!showTodos)
+  }
+  
+  function addTodo(todo){
+    setTodos([...todos, todo])
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>React To-Do</h1>
       </header>
+      <main>
+        <section>
+          <br/>
+          <button onClick={handleHideClick}>{showTodos ? "Hide" : "Show"}</button>
+        </section>
+        {/* Conditionally render TodoList */}
+        {showTodos && <ToDoList todos={todos} />}
 
-      {showTodos && <ToDoList todos={todos} />}
-
+        <NewTodoForm addTodo={addTodo}/>
+      </main>
     </div>
   );
 }
 
 export default App;
-
